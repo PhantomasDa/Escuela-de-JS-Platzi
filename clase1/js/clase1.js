5176e3ec0862d57a8c8e7d08ccbde8d91486ab07
Christian rios <christianrios@dada.com.ec>
2020-02-03 22:22:00 -0500

    clase 1 completada y comentada

--- clase1/js/clase1.js
+++ clase1/js/clase1.js
@@ -1,1 +1,13 @@
-console.log('hola mundo')
+// Importante las variables
+// en javascript son debilmente tipadas
+// es decir no sabemos el tipo de dato
+// que guarda
+
+var nombre = "Christian"
+var apellido = 'Rios'
+var edad = 25
+// Imprimimos el nombre
+console.log('hola ' + nombre + ' '+ apellido)
+console.log('tengo ' + edad +' años')
+
+var peso = 75
