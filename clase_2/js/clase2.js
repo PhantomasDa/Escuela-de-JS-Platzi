c79cf2dbeb3b34c1438203788ae744149b8aba2e
Christian rios <christianrios@dada.com.ec>
2020-02-04 09:45:37 -0500

    segunda clase

---
+++ clase_2/js/clase2.js
@@ -0,0 +1,16 @@
+var nombre = "Christian", apellido = 'Rios'
+// Siempre comillas simples para los string
+var nombreEnMayusculas= nombre.toUpperCase()
+
+var apellidoEnMiniscula = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+
+var nomvreCompleto = nombre + ' '+ apellido
+
+var nombrecompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1,2)
---
+++ clase_2/js/reto1.js
@@ -0,0 +1,7 @@
+var nombre;
+const c = console.log
+
+nombre=prompt('ingresa tu nombre')
+
+c(`su nombre es ${nombre}`)
+c(`la última letra de su nombre es ${nombre.charAt(nombre.length-1)}`)
