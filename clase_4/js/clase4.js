1e061441ede2f749ab7bf39d07b0239e94315daa
Christian rios <christianrios@dada.com.ec>
2020-02-05 12:56:33 -0500

    clase 4

---
+++ clase_4/js/clase4.js
@@ -0,0 +1,10 @@
+var nombre ='christian', edad = 23
+
+function imprimirEdad(n, e) {
+    console.log (`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('viky', 28)
+imprimirEdad('dario', 18)
+imprimirEdad('carlos', 28)
