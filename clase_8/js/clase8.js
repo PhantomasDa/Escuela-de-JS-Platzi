67794f38753f5955edc3eeceb38154a82dacd560
Christian rios <christianrios@dada.com.ec>
2020-02-10 18:21:58 -0500

    clase 8 Desestructurar objetos

---
+++ clase_8/js/clase8.js
@@ -0,0 +1,32 @@
+var sacha = {
+    nombre: 'christian',
+    Apellido : 'Mock',
+    edad: 19
+}
+
+var arturo = {
+    nombre: 'arturo',
+    Apellido : 'rios',
+    edad: 29
+}
+
+function impimirNombreEnMayuscula(persona){
+    // var nombre = persona.nombre
+    var {nombre} = persona
+    console.log(nombre.toUpperCase())
+}
+
+
+impimirNombreEnMayuscula(sacha)
+impimirNombreEnMayuscula(arturo)
+// impimirNombreEnMayuscula({nombre: 'pepito'})
+
+function imprimirNombreYEdad(persona){
+    var {nombre} = persona
+    var {edad} = persona
+    console.log(`hola me llamo ${nombre.} y tengo ${edad} años`)
+}
+
+imprimirNombreYEdad(arturo)
+imprimirNombreYEdad(sacha)
+
