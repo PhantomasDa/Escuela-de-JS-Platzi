4aea44d02ba39b3e9b40b0f655935c9c8182a791
Christian rios <christianrios@dada.com.ec>
2020-02-20 18:42:54 -0500

    comparaciones

---
+++ clase_10/js/clase10.js
@@ -0,0 +1,16 @@
+var x = 4, y= '4'
+
+//comparacion de objetos
+
+var sacha = {
+    nombre: 'sacha'
+}
+
+var otraPersona = {
+    nombre: 'sacha'
+
+}
+//puedo modificarlos  ejecutando
+var otraPersona = {
+    ...sacha
+}
