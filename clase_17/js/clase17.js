1e6418a13942791c96f72f75fdc4fdcad7bc4938
Christian rios <christianrios@dada.com.ec>
2020-02-27 16:46:59 -0500

    switch

---
+++ clase_17/js/clase17.js
@@ -0,0 +1,18 @@
+var signo = prompt( `¿Cuál es tu signo?`)
+
+console.log(`signo`)
+
+switch (signo) {
+    case 'Acuario':
+        console.log(`morias mañana `)
+        break;
+    
+        case 'Cancer':
+                console.log(`morias mañana 1212`)
+        break
+
+    default:
+        console.log(`escribiste mal el signo, idiota`)
+        break;
+}
+
