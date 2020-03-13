7fe37990e6ac52bd62554c0cbb4ab8932cb9d258
Christian rios <christianrios@dada.com.ec>
2020-03-13 11:16:33 -0500

    conexión a api

--- 6.Asincronismo/5.Haciendo multiples requests/js/clase.js
+++ 6.Asincronismo/5.Haciendo multiples requests/js/clase.js
@@ -1,22 +1,22 @@
-class Persona{
-  constructor(nombre , apellido ,altura){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.altura = altura;
-  }
-  saludar(){
-    console.log(`hola me llamo ${this.nombre} ${this.apellido} `);
-  }
-  soyAlto(){
-    return this.altura >1.75;
-  }
-}
-class desarrollador extends Persona{
-constructor(nombre, apellido, altura) {
-  super(nombre, apellido, altura)
-}
-saludar(){
-  console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador `);
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = {crossDomain: true}
 
+const onPeopleResponse = function (persona){
+  console.log(`Hola yo soy ${persona.name}`)
 }
+
+function obtenerPersonaje(id){
+const url=`${API_URL}${PEOPLE_URL.replace(':id', id)}`
+$.get(url, opts, onPeopleResponse)
 }
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
+obtenerPersonaje(4)
+obtenerPersonaje(5)
+obtenerPersonaje(6)
+obtenerPersonaje(7)
+obtenerPersonaje(8)
+obtenerPersonaje(9)
+
