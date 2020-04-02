07022c77dd298ea4935a27a3d5cb831e14d5c5da
Christian rios <christianrios@dada.com.ec>
2020-04-02 15:36:14 -0500

    async await aun no funciona

--- 6.Asincronismo/10.Múltiple promesas en paralelo/js/clase.js
+++ 6.Asincronismo/10.Múltiple promesas en paralelo/js/clase.js
@@ -1,22 +1,59 @@
-class Persona{
-  constructor(nombre , apellido ,altura){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.altura = altura;
-  }
-  saludar(){
-    console.log(`hola me llamo ${this.nombre} ${this.apellido} `);
-  }
-  soyAlto(){
-    return this.altura >1.75;
-  }
-}
-class desarrollador extends Persona{
-constructor(nombre, apellido, altura) {
-  super(nombre, apellido, altura)
-}
-saludar(){
-  console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador `);
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = {crossDomain: true}
 
+
+function obtenerPersonaje(id){
+return new Promise((resolve, reject) =>{
+  const url=`${API_URL}${PEOPLE_URL.replace(':id', id)}`
+$.get(url, opts, function(data){
+  resolve(data)
+})
+.fail(() => reject(id))
+})
 }
+function onError(id){
+  console.log(`lo siento, tenemos un error con el personaje ${id}`)
 }
+
+var ids = [1,2,3,4,5,6,7,8,9]
+// var promesas = ids.map(function(id){
+//   return obtenerPersonaje(id)
+// })
+
+var promesas = ids.map(id => obtenerPersonaje(id))
+Promise
+.all(promesas)
+.then(personajes  => console.log(personajes))
+.catch(onError)
+// obtenerPersonaje(1)
+// .then(personaje1 =>{
+//   console.log(`el personaje 1 es ${personaje1.name} `)
+//   return obtenerPersonaje(2)
+// })
+// .then(personaje2 =>{
+//   console.log(`el personaje 2 es ${personaje2.name} `)
+//   return obtenerPersonaje(3)
+// })
+// .then(personaje3 =>{
+//   console.log(`el personaje 3 es ${personaje3.name} `)
+//   return obtenerPersonaje(4)
+// })
+// .then(personaje4 => {
+//   console.log(`el personaje 4 es ${personaje4.name} `)
+//   return obtenerPersonaje(5)
+// })
+// .then(personaje5 => {
+//   console.log(`el personaje 5 es ${personaje5.name} `)
+//   return obtenerPersonaje(6)
+// })
+// .then(personaje6 => {
+//   console.log(`el personaje 6 es ${personaje6.name} `)
+//   return obtenerPersonaje(7)
+// })
+// .then(personaje7 => {
+//   console.log(`el personaje 7 es ${personaje7.name} `)
+// })
+
+// .catch(onError)
+
--- 6.Asincronismo/11.Async-await/js/clase.js
+++ 6.Asincronismo/11.Async-await/js/clase.js
@@ -1,22 +1,31 @@
-class Persona{
-  constructor(nombre , apellido ,altura){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.altura = altura;
-  }
-  saludar(){
-    console.log(`hola me llamo ${this.nombre} ${this.apellido} `);
-  }
-  soyAlto(){
-    return this.altura >1.75;
-  }
-}
-class desarrollador extends Persona{
-constructor(nombre, apellido, altura) {
-  super(nombre, apellido, altura)
-}
-saludar(){
-  console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador `);
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = {crossDomain: true}
 
+
+function obtenerPersonaje(id){
+return new Promise((resolve, reject) =>{
+  const url=`${API_URL}${PEOPLE_URL.replace(':id', id)}`
+$.get(url, opts, function(data){
+  resolve(data)
+})
+.fail(() => reject(id))
+})
 }
+function onError(id){
+  console.log(`lo siento, tenemos un error con el personaje ${id}`)
 }
+
+async function obtenerPersonaje(){
+var ids = [1,2,3,4,5,6,7,8,9]
+var promesas = ids.map(id => obtenerPersonaje(id))
+try{
+  var personaje = await Promise.all(promises)
+  console.log(personajes)
+}catch (id){
+    onError(id)
+ }
+}
+
+obtenerPersonaje()
+
